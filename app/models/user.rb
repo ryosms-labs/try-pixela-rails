4fc39a03e34c52ebd22ee1c4e9f66d0dc2f6900a
Ryo Sumasu <ryo.sms@gmail.com>
2018-11-29 17:42:34 +0900

    Impl User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,26 @@
 #
 
 class User < ApplicationRecord
+  validates :user_name, presence: true, uniqueness: { allow_blank: true }
+  validates :token, presence: true
+
+  before_save :encrypt_token
+
+  def encrypt_token
+    self.token = token_encoder.encrypt_and_sign(self.token)
+  end
+
+  def decrypt_token
+    token_encoder.decrypt_and_verify(self.token)
+  end
+
+  def token_encoder
+    cipher = 'aes-256-cbc'
+    secret = Rails.application.credentials.secret_key_base
+    # Saltはもうちょっとひねろうか
+    salt = Rails.application.secret_key_base
+    key_len = ActiveSupport::MessageEncryptor.key_len(cipher)
+    key = ActiveSupport::KeyGenerator.new(secret).generate_key(salt, key_len)
+    ActiveSupport::MessageEncryptor.new(key, cipher: cipher)
+  end
 end
