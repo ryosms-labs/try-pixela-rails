1ca6c62fd484c489e4e2c8ec6874e6957efd114d
Ryo Sumasu <ryo.sms@gmail.com>
2018-11-30 17:30:12 +0900

    Add color tag

--- app/helpers/graphs_helper.rb
+++ app/helpers/graphs_helper.rb
@@ -1,2 +1,20 @@
 module GraphsHelper
+  def graph_color_class(color)
+    case color
+    when 'shibafu'
+      'success'
+    when 'momiji'
+      'danger'
+    when 'sora'
+      'primary'
+    when 'ichou'
+      'warning'
+    when 'ajisai'
+      'info'
+    when 'kuro'
+      'dark'
+    else
+      'light'
+    end
+  end
 end
